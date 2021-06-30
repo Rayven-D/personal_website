a5ef2add0ccace4cf6113cbe3f1c5e8a0d1eba78
Rayven Deray <Rayven.Deray@ttu.edu>
2021-06-29 20:03:31 -0500

    first changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar';
+import { MatTabsModule, MatTabNav } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,10 @@ import { MatTabsModule } from '@angular/material/tabs';
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
+    MatTabNav,
     MatTabsModule,
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
