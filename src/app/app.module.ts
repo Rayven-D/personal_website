e14103f4b9405854eef7cdfd3e89bb846c5ac393
Rayven Deray <rjderay@gmail.com>
2021-07-03 20:54:32 -0500

    added pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar';
 import { MatTabsModule, MatTabNav } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { LinksComponent } from './pages/links/links.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ResumeComponent,
+    LinksComponent,
   ],
   imports: [
     BrowserModule,
