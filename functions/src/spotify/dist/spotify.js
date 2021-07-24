50ee305ad2599755b3e1da278015a5abb182f8e3
Rayven <rjderay@gmail.com>
2021-07-24 11:55:03 -0500

    removed spotify. will do it in another branch

--- functions/src/spotify/dist/spotify.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var functions = require("firebase-functions");
-var spotify_web_api_ts_1 = require("spotify-web-api-ts");
-var spotifyAPI = new spotify_web_api_ts_1.SpotifyWebApi({
-    clientId: functions.config().spotify.clientid,
-    clientSecret: functions.config().spotify.clientsecret
-});
--- functions/src/spotify/spotify.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from "firebase-functions";
-import { SpotifyWebApi } from "spotify-web-api-ts"
-
-const spotifyAPI = new SpotifyWebApi({
-    clientId: functions.config().spotify.clientid,
-    clientSecret: functions.config().spotify.clientsecret,
-});
-
