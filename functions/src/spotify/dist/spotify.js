11ec880aa5f4a028119e1aa7da9b69dac7dfb66f
Rayven <rjderay@gmail.com>
2021-07-24 02:59:23 -0500

    spotify quick fix

--- functions/src/spotify/dist/spotify.js
+++ functions/src/spotify/dist/spotify.js
@@ -1,7 +1,8 @@
 "use strict";
 exports.__esModule = true;
+var functions = require("firebase-functions");
 var spotify_web_api_ts_1 = require("spotify-web-api-ts");
 var spotifyAPI = new spotify_web_api_ts_1.SpotifyWebApi({
-    clientId: clientId,
-    clientSecret: clientSecret
+    clientId: functions.config().spotify.clientid,
+    clientSecret: functions.config().spotify.clientsecret
 });
--- functions/src/spotify/spotify.ts
+++ functions/src/spotify/spotify.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 import { SpotifyWebApi } from "spotify-web-api-ts"
 
 const spotifyAPI = new SpotifyWebApi({
-    clientId = functions.config().spotify.clientid,
-    clientSecret = functions.config().spotify.clientsecret,
+    clientId: functions.config().spotify.clientid,
+    clientSecret: functions.config().spotify.clientsecret,
 });
 
