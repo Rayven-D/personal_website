a9d13e27216236f521ffde3585f41a53be946d28
Rayven <rjderay@gmail.com>
2022-12-05 22:54:28 -0600

    RW-8 work in progress screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeScreenComponent } from './pages/welcome-screen/welcome-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeScreenComponent,
   ],
   imports: [
     BrowserModule,
